5729626f291d335e381242d6018c528a9cd0ce37
ammar1625 <ammarbouzenka25@gmail.com>
2024-10-31 11:39:55 +0100

    done with display movements

--- scripts/main-page.js
+++ scripts/main-page.js
@@ -6,7 +6,55 @@ let currentUser =JSON.parse(localStorage.getItem("User"))|| {};
 let headerUserNameEl  = document.querySelector(".user");
 let balanceEl  = document.querySelector(".ammount");
 
+//navigation side bad elements
+let depoAndWithdrawEl = document.querySelector(".depo-withd");
+let transactionsEl = document.querySelector(".transactions");
+let personalInfosEl = document.querySelector(".personal-infos");
+let closeAccoutEl = document.querySelector(".close-acc");
+let logoutEl = document.querySelector(".log-out");
 
+let tabs = document.querySelectorAll(".side-btn");
+
+
+/*tabs sections dom elements*/
+
+let depositWithdrawSectionEl = document.querySelector(".deposits-withdraws-ctr");
+let transactionsSectionsEl = document.querySelector(".transactions-ctr");
+let personalInfosSectionEl = document.querySelector(".personal-infos-ctr");
+let closeAccountSectionEl = document.querySelector(".close-account-ctr");
+
+/*dep and with dom elements*/
+
+let depositsAndWithdrawsHistoryContainerEl = document.querySelector(".dep-history-ctr");
+let depWithHistoryRecordEl = document.querySelector(".dep-history-el");
+let depMovementTypeEl = document.querySelector(".movement-type");
+let depMovementDateEl = document.querySelector(".movement-date");
+let depMovementAmmountEl = document.querySelector(".movement-ammount");
+
+function renderDepositsAndWithdrawsRecords()
+{
+    let html = "";
+    getDepositsAndWithdrawsList(currentAccount.accountId).then(function(movRes){
+
+        for(let mov of movRes)
+            {
+                let action = mov.transactionAmmount <0 ? "withdraw":"deposit";
+                
+                let html = `
+                     <div class="dep-history-el history-el">
+                                    <p class="movement-type">${action}</p>
+                                    <p class="movement-date">${formatDate(mov.transactionDate)}</p>
+                                    <p class="movement-ammount">${formatMoney(mov.transactionAmmount)} $</p>
+                                </div>
+                `;
+             
+                depositsAndWithdrawsHistoryContainerEl.insertAdjacentHTML("beforeend",html);
+               
+            }
+
+    })
+
+}
 
 async function getAccountByUserId(userId)
 {
@@ -60,6 +108,11 @@ function formatMoney(ammount)
     return ammount.toFixed(2);
 }
 
+function formatDate(date)
+{
+    return date.replace("T"," / ").slice(0,-3);
+}
+
 function updateBalance()
 {
     getBalanceByAccountId(currentAccount.accountId).then(function(res){
@@ -78,4 +131,101 @@ function loadHeadersData()
     updateUserFirstName();
 }
 
+function markTabSelected()
+{
+    tabs.forEach(function(tab){
+        tab.addEventListener("click",function(e){
+
+            tabs.forEach((t)=>{
+                t.classList.remove("selected");
+            })
+
+            e.target.classList.add("selected");
+        })
+    });
+
+
+}
+
+function displaySection(section)
+{
+    section.classList.remove("hidden");
+}
+
+function hideSection(section)
+{
+    section.classList.add("hidden");
+}
+
+function navigateThroughSections()
+{
+    depoAndWithdrawEl.addEventListener("click",()=>{
+        displaySection(depositWithdrawSectionEl);
+
+        hideSection(transactionsSectionsEl);
+        hideSection(personalInfosSectionEl);
+        hideSection(closeAccountSectionEl);
+
+    });
+
+    transactionsEl.addEventListener("click",()=>{
+        displaySection(transactionsSectionsEl);
+
+        hideSection(depositWithdrawSectionEl);
+        hideSection(personalInfosSectionEl);
+        hideSection(closeAccountSectionEl);
+
+    });
+
+    personalInfosEl.addEventListener("click",()=>{
+        displaySection(personalInfosSectionEl);
+
+        hideSection(transactionsSectionsEl);
+        hideSection(depositWithdrawSectionEl);
+        hideSection(closeAccountSectionEl);
+
+    });
+
+    closeAccoutEl.addEventListener("click",()=>{
+        displaySection(closeAccountSectionEl);
+
+        hideSection(transactionsSectionsEl);
+        hideSection(personalInfosSectionEl);
+        hideSection(depositWithdrawSectionEl);
+
+    });
+}
+
+async function getDepositsAndWithdrawsList(accountId)
+{
+    try
+    {
+        let depWithResponse = await
+         fetch(`http://localhost:5104/api/Transactions/GetDepositsAndWithdraws?AccountId=${accountId}`);
+
+         if(!depWithResponse.ok)
+            {
+                throw new Error(`${depWithResponse.statusText}`);
+            }
+        let data = depWithResponse.json();
+
+        return data;
+    }
+    catch(err)
+    {
+
+    }
+
+    return null;
+}
+
 loadHeadersData();
+markTabSelected();
+navigateThroughSections();
+renderDepositsAndWithdrawsRecords();
+/* let trans = await getDepositsAndWithdrawsList(currentAccount.accountId);
+
+for(let tra of trans)
+    {
+        console.log(tra);
+    } */
